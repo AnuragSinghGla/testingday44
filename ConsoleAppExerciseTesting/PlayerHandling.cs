48b622dfb698e5cd2f55d79fa570178521b67144
Anurag.singh_cs16@gla.ac.in <anurag.singh_cs16@gla.ac.in>
2021-03-23 11:21:47 +0530

    final

---
+++ ConsoleAppExerciseTesting/PlayerHandling.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExerciseTesting
+{
+    [TestFixture]
+    public class PlayerHandling
+    {
+        [Test]
+        public void TestOne()
+        {
+            foreach (var Player in Program.Playerlist())
+            {
+                Assert.IsNotNull(Player.PlayerId);
+                Assert.IsNotNull(Player.PlayerName);
+                Assert.IsNotNull(Player.PlayerTeam);
+
+            }
+        }
+    }
+}
+        
---
+++ ConsoleAppExerciseTesting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExerciseTesting
+{
+    class Program
+    {
+        public static List<Player> Playerlist()
+        {
+            List<Player> plist = new List<Player>
+            {
+                new Player{PlayerId=2,PlayerName="Dhoni",PlayerTeam="India"},
+                new Player{PlayerId=3,PlayerName="kohali",PlayerTeam="India"},
+                new Player{PlayerName="raina",PlayerTeam="India"}
+               
+            };
+
+            return plist;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
